c0d6dba65f616787fe3a828bb6c6b786e5621146
Elif <elifnur.telase@gmail.com>
2021-12-30 14:04:29 +0200

    Kalitim ornegi tamamlandi.

---
+++ OOP/Inheritance/Akademisyen.java
@@ -0,0 +1,18 @@
+
+public class Akademisyen extends Staff{
+
+    private String bolum;
+    private String unvan;
+
+    Akademisyen(String staffName, String staffPhone, String staffEmail,String bolum,String unvan) {
+        super(staffName, staffPhone, staffEmail);
+        this.bolum = bolum;
+        this.unvan = unvan;
+    }
+    
+    public void derseGir()
+    {
+        System.out.println(this.getStaffName()+" derse giriş yaptı.");
+    }
+        
+}
---
+++ OOP/Inheritance/Memur.java
@@ -0,0 +1,17 @@
+public class Memur extends Staff {
+
+    private String departman;
+    private String mesai;
+    Memur(String staffName, String staffPhone, String staffEmail,String departman,String mesai) {
+        super(staffName, staffPhone, staffEmail);
+        this.departman=departman;
+        this.mesai=mesai;
+        //TODO Auto-generated constructor stub
+    }
+
+    public void calis()
+    {
+        System.out.println(this.getStaffName()+"calisiyor.");
+    }
+    
+}
