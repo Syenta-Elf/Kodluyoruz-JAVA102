4a851c9832fc2e7fd37717a82c9c2808a4b7b475
Elif <elifnur.telase@gmail.com>
2021-12-30 11:37:07 +0200

    Kapsülleme örneği tamamlandı.

---
+++ OOP/Encapsulation/Book.java
@@ -0,0 +1,34 @@
+public class Book {
+
+    private String bookName;
+    private int pageNumber;
+
+    public Book(String bookName , int pageNumber)
+    {
+        this.bookName = bookName;
+        if(pageNumber <= 0)
+        this.pageNumber = 0;
+        
+        else
+        this.pageNumber = pageNumber;
+    }
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+    public void setPageNumber(int pageNumber) {
+        if(pageNumber <= 0)
+        this.pageNumber = 0;
+
+        else
+        this.pageNumber = pageNumber;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+    
+}
---
+++ OOP/Encapsulation/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+        public static void main(String[] args) {
+            Book kitap = new Book("kitap1", -1233);
+            System.out.println(kitap.getBookName());
+            System.out.println(kitap.getPageNumber());
+        }
+}
