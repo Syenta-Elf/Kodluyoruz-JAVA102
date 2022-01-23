48932b97f5138e361519d6dc42b155efc53aad36
Elif <elifnur.telase@gmail.com>
2022-01-23 12:47:08 +0200

    Wrapper ornekleri tamamlandi.

---
+++ NestedSiniflar-GenericYapilar/WrapperSiniflar/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        String a = "20";
+        int b = 10;
+        int c = Integer.parseInt(a);
+        System.out.println(c+b+a);
+
+        System.out.println(Integer.parseInt(a)+b);
+    }    
+}
