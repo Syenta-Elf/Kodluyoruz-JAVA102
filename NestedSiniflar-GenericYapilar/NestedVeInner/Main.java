4ec1f89f7a43abbd99cb308e5b19df8587cf0a72
Elif <elifnur.telase@gmail.com>
2022-01-23 12:30:30 +0200

    Anonim ornegi eklendi.

--- NestedSiniflar-GenericYapilar/NestedVeInner/Main.java
+++ NestedSiniflar-GenericYapilar/NestedVeInner/Main.java
@@ -10,5 +10,21 @@ public class Main {
         //oi.run();
         Locale locale = new Locale();
         locale.run();
+
+        Anonim an = new Anonim(){
+            public int a=5;
+
+            @Override
+            public void run() {
+                System.out.println("Override anonim");
+                System.out.println(a);
+                print();
+            };
+            public void print()
+            {
+                System.out.println("deneme");
+            }
+        };
+        an.run();
     }
 }
