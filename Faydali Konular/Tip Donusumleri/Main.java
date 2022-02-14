9d97dd9bf1cfcd80318f3b27a93e37ca929391d5
Elif <elifnur.telase@gmail.com>
2022-02-14 16:54:15 +0200

    Tip donusumleri ornegi eklendi.

---
+++ Faydali Konular/Tip Donusumleri/Main.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[] args) {
+        int a = 20;
+        String b = String.valueOf(a);
+
+        System.out.println((char)a);
+
+        System.out.println(Integer.parseInt(b)+a);
+    }
+}
